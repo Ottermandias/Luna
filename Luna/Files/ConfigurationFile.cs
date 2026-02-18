3fa2195ba9b34c4ce21e3180bf238edb488a4d35
Ottermandias <nickroederspam+git1@gmail.com>
2026-02-18 16:33:04 +0100

    Move ConfigurationFile to Luna.

---
+++ Luna/Files/ConfigurationFile.cs
@@ -0,0 +1,79 @@
+using Dalamud.Interface.ImGuiNotification;
+
+namespace Luna;
+
+/// <summary> A base class for efficient configuration files using the Luna saving subsystems. </summary>
+/// <typeparam name="TProvider"> The type of file name provider this file uses. </typeparam>
+/// <param name="saveService"> The actual save service this file uses. </param>
+/// <param name="messager"> The file name provider this file uses. </param>
+/// <param name="saveDelay"> An optional delay for saving this file. If this is null, a minute is used. </param>
+public abstract class ConfigurationFile<TProvider>(BaseSaveService<TProvider> saveService, MessageService messager, TimeSpan? saveDelay = null)
+    : ISavable<TProvider>, IService
+    where TProvider : BaseFilePathProvider
+{
+    /// <summary> The save service this file uses. </summary>
+    protected readonly BaseSaveService<TProvider> SaveService = saveService;
+
+    /// <summary> The messager this file uses. </summary>
+    protected readonly MessageService Messager = messager;
+
+    /// <summary> The current version of this file in the program. </summary>
+    public abstract int CurrentVersion { get; }
+
+    /// <summary> The function that writes this file's specific data to the JSON stream. </summary>
+    /// <param name="j"> The JSON writer. </param>
+    protected abstract void AddData(JsonTextWriter j);
+
+    /// <summary> The function that parses the JObject and fills this files data with the result. </summary>
+    /// <param name="j"> The deserialized JObject. </param>
+    protected abstract void LoadData(JObject j);
+
+    /// <summary> Obtain this file's path for saving or loading from the file name provider. </summary>
+    /// <returns> The full path for this file. </returns>
+    public abstract string ToFilePath(TProvider fileNames);
+
+    /// <summary> An optional delay for saving this file. </summary>
+    protected TimeSpan SaveDelay { get; set; } = saveDelay ?? TimeSpan.FromMinutes(1);
+
+    /// <summary> Save this file using the stored delay. </summary>
+    public virtual void Save()
+        => SaveService.DelaySave(this, SaveDelay);
+
+    /// <summary> Save this file with common data, indentation and using <see cref="AddData"/>. </summary>
+    /// <param name="writer"> The stream writer to write to.</param>
+    public virtual void Save(StreamWriter writer)
+    {
+        using var j = new JsonTextWriter(writer);
+        j.Formatting = Formatting.Indented;
+        j.WriteStartObject();
+        j.WritePropertyName("Version");
+        j.WriteValue(CurrentVersion);
+        AddData(j);
+        j.WriteEndObject();
+    }
+
+    /// <summary> Load this file from its default file path if it exists using <see cref="LoadData"/> and notify on errors. </summary>
+    protected virtual void Load()
+    {
+        var fileName = ToFilePath(SaveService.FileNames);
+        var logName  = ((ISavable<TProvider>)this).LogName(fileName);
+        if (!File.Exists(fileName))
+            return;
+
+        try
+        {
+            Messager.Log.Debug($"Reading {logName}...");
+            var text = File.ReadAllText(fileName);
+            var jObj = JObject.Parse(text);
+            if (jObj["Version"]?.Value<int>() != CurrentVersion)
+                throw new Exception("Unsupported version.");
+
+            LoadData(jObj);
+        }
+        catch (Exception ex)
+        {
+            Messager.NotificationMessage(ex, $"Error reading {logName}, reverting to default.",
+                $"Error reading {logName}", NotificationType.Error);
+        }
+    }
+}
