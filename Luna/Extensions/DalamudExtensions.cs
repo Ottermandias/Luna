b79c8720d5a5911eba9d56c06d980b9951380aaf
Ottermandias <nickroederspam+git1@gmail.com>
2026-04-01 16:16:14 +0200

    Add an extension for scaling of textures.

--- Luna/Extensions/DalamudExtensions.cs
+++ Luna/Extensions/DalamudExtensions.cs
@@ -1,5 +1,6 @@
 using Dalamud.Bindings.ImGui;
 using Dalamud.Interface.ImGuiFileDialog;
+using Dalamud.Interface.Textures.TextureWraps;
 
 namespace Luna;
 
@@ -17,4 +18,38 @@ public static class DalamudExtensions
     /// <returns> A file dialog manager. </returns>
     public static FileDialogManager CreateFileDialog(WindowFlags flags = WindowFlags.None)
         => new() { AddedWindowFlags = flags.ToDalamudWindowFlags() };
+
+    /// <summary> Scale the size of an image down to fit a maximum size in both dimensions. </summary>
+    /// <param name="wrap"> The image provided as a Dalamud wrap. </param>
+    /// <param name="maxSize"> The maximum size. </param>
+    /// <returns> The size of the image scaled down to the maximum size if necessary, with preserved ratio. </returns>
+    public static Vector2 ScaledDownSize(this IDalamudTextureWrap wrap, float maxSize)
+    {
+        var size = wrap.Size;
+        if (wrap.Size.X >= maxSize)
+        {
+            if (wrap.Size.Y < wrap.Size.X)
+            {
+                size.X = maxSize;
+                size.Y = maxSize / wrap.Size.X * wrap.Size.Y;
+            }
+            else if (wrap.Size.Y == wrap.Size.X)
+            {
+                size.X = maxSize;
+                size.Y = maxSize;
+            }
+            else
+            {
+                size.Y = maxSize;
+                size.X = maxSize / wrap.Size.Y * wrap.Size.X;
+            }
+        }
+        else if (wrap.Size.Y >= maxSize)
+        {
+            size.Y = maxSize;
+            size.X = maxSize / wrap.Size.Y * wrap.Size.X;
+        }
+
+        return size;
+    }
 }
