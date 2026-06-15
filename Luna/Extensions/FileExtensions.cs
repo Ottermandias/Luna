36b3acffd0328ff68b72f1c40ffeae71fb58f63f
Ottermandias <nickroederspam+git1@gmail.com>
2026-06-15 14:35:05 +0200

    Add Path.CombineSafely.

--- Luna/Extensions/FileExtensions.cs
+++ Luna/Extensions/FileExtensions.cs
@@ -20,6 +20,28 @@ public static class FileExtensions
     public static bool IsHidden(this FileSystemInfo file)
         => file.Attributes.HasFlag(FileAttributes.Hidden);
 
+    extension(Path)
+    {
+        /// <summary> Combine a base directory and a relative path in a way that does not allow the resulting path to lie outside the base directory. </summary>
+        /// <param name="directory"> The base directory. </param>
+        /// <param name="relativePath"> The relative path. </param>
+        /// <param name="comparison"> The comparison method to use to check whether the resulting path lies in the base directory. </param>
+        /// <returns> The normalized full path. </returns>
+        /// <exception cref="UnauthorizedAccessException"> If the resulting full path is outside the base directory. </exception>
+        /// <remarks> Does not prevent junctions or links. </remarks>
+        public static string CombineSafely(string directory, string relativePath,
+            StringComparison comparison = StringComparison.OrdinalIgnoreCase)
+        {
+            var normalizedDirectory = Path.GetFullPath(directory);
+            if (!normalizedDirectory.EndsWith(Path.DirectorySeparatorChar))
+                normalizedDirectory += Path.DirectorySeparatorChar;
+            var normalizedPath = Path.GetFullPath(relativePath, normalizedDirectory);
+            if (!normalizedPath.StartsWith(normalizedDirectory, comparison))
+                throw new UnauthorizedAccessException($"Path '{relativePath}' escapes base directory '{directory}'.");
+
+            return normalizedPath;
+        }
+    }
 
     private static void EnumerateNonHiddenFilesRecurse(List<FileInfo> files, FileSystemInfo info, Func<FileInfo, bool>? filter)
     {
