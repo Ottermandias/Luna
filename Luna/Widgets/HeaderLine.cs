33977ffed6afefc3d5e8ddf04eca3ee1926f42a9
Ottermandias <nickroederspam+git1@gmail.com>
2026-07-20 21:57:54 +0200

    Add tooltips icon and option to hide the rightmost line.

--- Luna/Widgets/HeaderLine.cs
+++ Luna/Widgets/HeaderLine.cs
@@ -23,6 +23,9 @@ public struct HeaderLine
     /// <summary> The color of the label text when the header is expanded. </summary>
     public ColorParameter TextColorExpanded;
 
+    /// <summary> The color for the tooltip icon, if any. </summary>
+    public ColorParameter TooltipIconColor;
+
     /// <summary> The background color of the label and expansion button when expanded. </summary>
     public ColorParameter ButtonBackgroundExpanded;
 
@@ -34,6 +37,9 @@ public struct HeaderLine
 
     /// <summary> The background color of the label and expansion button when it is clicked. </summary>
     public ColorParameter ButtonActive;
+
+    /// <summary> The icon to use if the header has a tooltip, if any. </summary>
+    public AwesomeIcon TooltipIcon;
 
     /// <summary> If this is 0 the label and expansion button auto-fits to the text and caret. Otherwise, this provides a minimum width. </summary>
     public float FixedButtonWidth;
@@ -65,6 +71,9 @@ public struct HeaderLine
     /// <summary> When this is true, no header, label or collapse button will be drawn at all. The distances will be computed using FixedButtonWidth. </summary>
     /// <remarks> This is not respected for <see cref="Basic"/>, as that would just be a separator. </remarks>
     public bool NoLabel;
+
+    /// <summary> Whether the rightmost part of the line is to be drawn or not. </summary>
+    public bool HideRightLine;
 
     /// <summary> Draw a header line with a label and expansion button as well as a custom combo. </summary>
     /// <typeparam name="TCacheItem"> The combo item type. </typeparam>
@@ -121,52 +130,18 @@ public struct HeaderLine
         if (!ImEx.SplitLabel(ref label, out var visible, out var id))
             return false;
 
-        var expanded = !Collapsible || Im.State.Storage.GetBool(id, !DefaultClosed);
-        var shapes   = Im.Window.DrawList.Shape;
+        using var _        = Im.Id.Push(id);
+        var       expanded = !Collapsible || Im.State.Storage.GetBool(id, !DefaultClosed);
+        var (separatorColor, textColor, buttonBackground, caret) = GetColors(expanded);
+        var (frameHeight, lineThickness, linePosition)           = GetLineData();
+        var tooltipSpan = tooltip.GetSpan(out var s) ? s : StringU8.Empty;
+        var (textWidth, tooltipIconWidth, buttonWidth) = GetButtonWidth(visible, tooltipSpan);
+        DrawTwoLines(separatorColor, lineThickness, linePosition, buttonWidth);
 
-        // Colors
-        var (separatorColor, textColor, icon, buttonBackground) = expanded
-            ? (LineColorExpanded.CheckDefault(ImGuiColor.Separator), TextColorExpanded.CheckDefault(ImGuiColor.Text),
-                Direction.Down, ButtonBackgroundExpanded.CheckDefault(ImGuiColor.FrameBackground))
-            : (LineColorCollapsed.CheckDefault(ImGuiColor.Separator), TextColorCollapsed.CheckDefault(ImGuiColor.Text),
-                Direction.Right, ButtonBackgroundCollapsed.CheckDefault(ImGuiColor.FrameBackground));
-
-        // Frame
-        var frameHeight     = Im.Style.FrameHeight;
-        var frameHeightEven = float.IsEvenInteger(frameHeight);
-        var (lineThickness, linePosition) = frameHeightEven ? (2, frameHeight / 2) : (1, (frameHeight - 1) / 2);
-
-        // Button Size
-        var textWidth = GetButtonWidth(visible);
-
-        // Draw Lines.
-        var startPos = Im.Window.Position with { Y = Im.Cursor.ScreenY + linePosition };
-        var fullEnd  = Im.Cursor.ScreenX + Im.ContentRegion.Available.X;
-        var endPos   = startPos with { X = Im.Cursor.ScreenX + LeftDistance };
-        shapes.Line(startPos, endPos, separatorColor, lineThickness);
-        startPos.X = endPos.X + textWidth;
-        endPos.X   = fullEnd;
-        shapes.Line(startPos, endPos, separatorColor, lineThickness);
-
-        using (ImStyleBorder.Frame.Push(separatorColor, lineThickness)
-                   .PushX(ImStyleDouble.ButtonTextAlign, 1)
-                   .Push(ImGuiColor.Button,        buttonBackground)
-                   .Push(ImGuiColor.ButtonHovered, ButtonHovered.CheckDefault(ImGuiColor.FrameBackgroundHovered))
-                   .Push(ImGuiColor.ButtonActive,  ButtonActive.CheckDefault(ImGuiColor.FrameBackgroundActive))
-                   .Push(ImGuiColor.Text,          textColor))
+        using (PushData(separatorColor, lineThickness, buttonBackground))
         {
-            Im.Cursor.X += LeftDistance;
-            if (Collapsible)
-            {
-                if (Im.Button(visible, new Vector2(textWidth, frameHeight)))
-                    Im.State.Storage.SetBool(id, !expanded);
-                var caretPosition = Im.Item.UpperLeftCorner + Im.Style.FramePadding;
-                Im.Window.DrawList.Render.Arrow(caretPosition, textColor, icon, 1f);
-            }
-            else
-            {
-                ImEx.TextFramed(visible, new Vector2(textWidth, frameHeight), buttonBackground);
-            }
+            DrawLabel(visible, tooltipSpan, id, tooltipIconWidth, textWidth, buttonWidth, 0, frameHeight, textColor, buttonBackground, caret,
+                false);
         }
 
         Im.Tooltip.OnHover(tooltip);
@@ -174,96 +149,41 @@ public struct HeaderLine
         return expanded;
     }
 
+    private Im.ColorStyleDisposable PushData(Rgba32 separator, float lineThickness, Rgba32 buttonBackground)
+        => ImStyleBorder.Frame.Push(separator, lineThickness)
+            .Push(ImGuiColor.Button,        buttonBackground)
+            .Push(ImGuiColor.ButtonHovered, ButtonHovered.CheckDefault(ImGuiColor.FrameBackgroundHovered))
+            .Push(ImGuiColor.ButtonActive,  ButtonHovered.CheckDefault(ImGuiColor.FrameBackgroundActive));
+
+
     [MethodImpl(ImSharpConfiguration.OptInl)]
     private (bool Expanded, bool Changed, ImGuiId ComboId, Rectangle ComboBoundingBox, TCacheItem? Selection) ComboInternal<TCacheItem>(
-        DrawCombo<TCacheItem>? drawer, float width, ref Utf8LabelHandler label, ref Utf8TextHandler tooltip,
-        ref Utf8HintHandler preview)
+        DrawCombo<TCacheItem>? drawer, float width, ref Utf8LabelHandler label, ref Utf8TextHandler tooltip, ref Utf8HintHandler preview)
     {
         if (!ImEx.SplitLabel(ref label, out var visible, out var id))
             return (false, false, 0, default, default);
 
         using var _        = Im.Id.Push(id);
         var       expanded = !Collapsible || Im.State.Storage.GetBool(id, !DefaultClosed);
-        var       shapes   = Im.Window.DrawList.Shape;
-
-        // Colors
-        var (separatorColor, textColor, icon, buttonBackground) = expanded
-            ? (LineColorExpanded.CheckDefault(ImGuiColor.Separator), TextColorExpanded.CheckDefault(ImGuiColor.Text),
-                Direction.Down, ButtonBackgroundExpanded.CheckDefault(ImGuiColor.FrameBackground))
-            : (LineColorCollapsed.CheckDefault(ImGuiColor.Separator), TextColorCollapsed.CheckDefault(ImGuiColor.Text),
-                Direction.Right, ButtonBackgroundCollapsed.CheckDefault(ImGuiColor.FrameBackground));
-
-        // Frame
-        var available       = Im.ContentRegion.Available;
-        var frameHeight     = Im.Style.FrameHeight;
-        var frameHeightEven = float.IsEvenInteger(frameHeight);
-        var (lineThickness, linePosition) = frameHeightEven ? (2, frameHeight / 2) : (1, (frameHeight - 1) / 2);
-
-        // Button Size
-        var textWidth = NoLabel ? FixedButtonWidth : GetButtonWidth(visible);
-
-        // Combo Size
-        var comboWidth = width > 0 ? width : Im.Font.CalculateSize(ref preview, false).X + 2 * Im.Style.FramePadding.X + Im.Style.FrameHeight;
-        if (FixedComboWidth is not 0 && FixedComboWidth > comboWidth)
-            comboWidth = FixedComboWidth;
-        var totalWidth = textWidth + comboWidth + LeftDistance + Math.Max(RightDistance, 0) + ComboDistance;
-        var centerDistance = totalWidth >= available.X ? ComboDistance :
-            RightDistance >= 0                         ? ComboDistance + available.X - totalWidth : ComboDistance;
-
-        // Draw Lines.
-        var startPos = Im.Window.Position with { Y = Im.Cursor.ScreenY + linePosition };
-        var endPos   = startPos with { X = Im.Cursor.ScreenX + LeftDistance };
-        if (NoLabel)
-        {
-            endPos.X = endPos.X + textWidth + centerDistance;
-        }
-        else
-        {
-            shapes.Line(startPos, endPos, separatorColor, lineThickness);
-            startPos.X = endPos.X + textWidth;
-            endPos.X   = startPos.X + centerDistance;
-        }
-
-        shapes.Line(startPos, endPos, separatorColor, lineThickness);
-        startPos.X = endPos.X + comboWidth;
-        endPos.X   = Im.Cursor.ScreenX + available.X;
-        shapes.Line(startPos, endPos, separatorColor, lineThickness);
+        var (separatorColor, textColor, buttonBackground, caret) = GetColors(expanded);
+        var (frameHeight, lineThickness, linePosition)           = GetLineData();
+        var tooltipSpan = tooltip.GetSpan(out var s) ? s : StringU8.Empty;
+        var (textWidth, tooltipIconWidth, buttonWidth) = GetButtonWidth(visible, tooltipSpan);
+        var (comboWidth, centerDistance)               = GetComboWidth(frameHeight, width, buttonWidth, ref preview);
+        DrawThreeLines(separatorColor, lineThickness, linePosition, buttonWidth, comboWidth, centerDistance);
 
         ImGuiId     popupId;
         Rectangle   boundingBox;
         bool        change;
         TCacheItem? selection;
-        using (var style = ImStyleBorder.Frame.Push(separatorColor, lineThickness)
-                   .Push(ImGuiColor.Text, textColor)
-                   .PushX(ImStyleDouble.ButtonTextAlign, 1)
-                   .Push(ImGuiColor.Button,        buttonBackground)
-                   .Push(ImGuiColor.ButtonHovered, ButtonHovered.CheckDefault(ImGuiColor.FrameBackgroundHovered))
-                   .Push(ImGuiColor.ButtonActive,  ButtonActive.CheckDefault(ImGuiColor.FrameBackgroundActive)))
+        using (var style = PushData(separatorColor, lineThickness, buttonBackground))
         {
             using var group = Im.Group();
-            Im.Cursor.X += LeftDistance;
+            DrawLabel(visible,    tooltipSpan, id, tooltipIconWidth, textWidth, buttonWidth, centerDistance, frameHeight, textColor,
+                buttonBackground, caret,       NoLabel);
+            style.PopColor(3);
             if (!NoLabel)
-            {
-                if (Collapsible)
-                {
-                    if (Im.Button(visible, new Vector2(textWidth, frameHeight)))
-                        Im.State.Storage.SetBool(id, !expanded);
-                    var caretPosition = Im.Item.UpperLeftCorner + Im.Style.FramePadding;
-                    Im.Window.DrawList.Render.Arrow(caretPosition, textColor, icon, 1f);
-                }
-                else
-                {
-                    ImEx.TextFramed(visible, new Vector2(textWidth, frameHeight), buttonBackground);
-                }
-
                 Im.Line.Same(0, centerDistance);
-            }
-            else
-            {
-                Im.Cursor.X += centerDistance + textWidth;
-            }
-
-            style.PopColor(3).PopStyle();
             using var disabled = Im.Disabled(ComboDisabled);
             if (drawer is null)
             {
@@ -285,13 +205,130 @@ public struct HeaderLine
         return (expanded, change, popupId, boundingBox, selection);
     }
 
-    private float GetButtonWidth(ReadOnlySpan<byte> text)
+    [MethodImpl(ImSharpConfiguration.OptInl)]
+    private (float Text, float Tooltip, float Button) GetButtonWidth(ReadOnlySpan<byte> text, ReadOnlySpan<byte> tooltip)
     {
-        var textWidth = Im.Font.CalculateSize(text, false).X + 2 * Im.Style.FramePadding.X;
+        var textWidth    = Im.Font.CalculateSize(text, false).X;
+        var buttonWidth  = Im.Font.CalculateSize(text, false).X + 2 * Im.Style.FramePadding.X;
+        var tooltipWidth = 0f;
         if (Collapsible)
-            textWidth += Im.Style.TextHeight + Im.Style.ItemInnerSpacing.X;
+            buttonWidth += Im.Style.TextHeight + Im.Style.ItemInnerSpacing.X;
+        if (!tooltip.IsEmpty && !TooltipIcon.IsEmpty)
+            buttonWidth += (tooltipWidth = TooltipIcon.CalculateSize().X) + Im.Style.ItemInnerSpacing.X;
         if (FixedButtonWidth is not 0 && FixedButtonWidth > textWidth)
-            textWidth = FixedButtonWidth;
-        return textWidth;
+            buttonWidth = FixedButtonWidth;
+        return (textWidth, tooltipWidth, buttonWidth);
+    }
+
+    [MethodImpl(ImSharpConfiguration.OptInl)]
+    private (Rgba32 Separator, Rgba32 Text, Rgba32 Button, Direction Caret) GetColors(bool expanded)
+        => expanded
+            ? (LineColorExpanded.CheckDefault(ImGuiColor.Separator), TextColorExpanded.CheckDefault(ImGuiColor.Text),
+                ButtonBackgroundExpanded.CheckDefault(ImGuiColor.FrameBackground),
+                Direction.Down)
+            : (LineColorCollapsed.CheckDefault(ImGuiColor.Separator), TextColorCollapsed.CheckDefault(ImGuiColor.Text),
+                ButtonBackgroundCollapsed.CheckDefault(ImGuiColor.FrameBackground),
+                Direction.Right);
+
+    [MethodImpl(ImSharpConfiguration.OptInl)]
+    private (float FrameHeight, float Thickness, float Position) GetLineData()
+    {
+        var frameHeight = Im.Style.FrameHeight;
+        return float.IsEvenInteger(frameHeight) ? (frameHeight, 2, frameHeight / 2) : (frameHeight, 1, (frameHeight - 1) / 2);
+    }
+
+    [MethodImpl(ImSharpConfiguration.OptInl)]
+    public (float Combo, float Center) GetComboWidth(float frameHeight, float width, float buttonWidth, ref Utf8HintHandler preview)
+    {
+        var available  = Im.ContentRegion.Available;
+        var comboWidth = width > 0 ? width : Im.Font.CalculateSize(ref preview, false).X + 2 * Im.Style.FramePadding.X + Im.Style.FrameHeight;
+        if (FixedComboWidth is not 0 && FixedComboWidth > comboWidth)
+            comboWidth = FixedComboWidth;
+        var totalWidth = buttonWidth + comboWidth + LeftDistance + Math.Max(RightDistance, 0) + ComboDistance;
+        var centerDistance = totalWidth >= available.X ? ComboDistance :
+            RightDistance >= 0                         ? ComboDistance + available.X - totalWidth : ComboDistance;
+        return (comboWidth, centerDistance);
+    }
+
+    [MethodImpl(ImSharpConfiguration.OptInl)]
+    private void DrawTwoLines(Rgba32 separatorColor, float lineThickness, float linePosition, float buttonWidth)
+    {
+        var drawList = Im.Window.DrawList.Shape;
+        var startPos = Im.Window.Position with { Y = Im.Cursor.ScreenY + linePosition };
+        var fullEnd  = Im.Cursor.ScreenX + Im.ContentRegion.Available.X;
+        var endPos   = startPos with { X = Im.Cursor.ScreenX + LeftDistance };
+        drawList.Line(startPos, endPos, separatorColor, lineThickness);
+        if (HideRightLine)
+            return;
+
+        startPos.X = endPos.X + buttonWidth;
+        endPos.X   = fullEnd;
+        drawList.Line(startPos, endPos, separatorColor, lineThickness);
+    }
+
+    [MethodImpl(ImSharpConfiguration.OptInl)]
+    private void DrawThreeLines(Rgba32 separatorColor, float lineThickness, float linePosition, float buttonWidth, float comboWidth,
+        float centerDistance)
+    {
+        var drawList  = Im.Window.DrawList.Shape;
+        var available = Im.ContentRegion.Available;
+        var startPos  = Im.Window.Position with { Y = Im.Cursor.ScreenY + linePosition };
+        var endPos    = startPos with { X = Im.Cursor.ScreenX + LeftDistance };
+        if (NoLabel)
+        {
+            endPos.X = endPos.X + buttonWidth + centerDistance;
+        }
+        else
+        {
+            drawList.Line(startPos, endPos, separatorColor, lineThickness);
+            startPos.X = endPos.X + buttonWidth;
+            endPos.X   = startPos.X + centerDistance;
+        }
+
+        drawList.Line(startPos, endPos, separatorColor, lineThickness);
+        if (HideRightLine)
+            return;
+        startPos.X = endPos.X + comboWidth;
+        endPos.X   = Im.Cursor.ScreenX + available.X;
+        drawList.Line(startPos, endPos, separatorColor, lineThickness);
+    }
+
+    [MethodImpl(ImSharpConfiguration.OptInl)]
+    private void DrawLabel(ReadOnlySpan<byte> text, ReadOnlySpan<byte> tooltip, ImGuiId id, float tooltipIconWidth, float textWidth,
+        float buttonWidth,
+        float centerDistance, float frameHeight, Rgba32 textColor, Rgba32 buttonBackground, Direction caret, bool noLabel)
+    {
+        Im.Cursor.X += LeftDistance;
+        if (!noLabel)
+        {
+            if (Collapsible)
+            {
+                if (Im.Button("##b"u8, new Vector2(buttonWidth, frameHeight)))
+                    Im.State.Storage.SetBool(id, caret is Direction.Right);
+                var caretPosition = Im.Item.UpperLeftCorner + Im.Style.FramePadding;
+                Im.Window.DrawList.Render.Arrow(caretPosition, textColor, caret, 1f);
+            }
+            else
+            {
+                ImEx.TextFramed(StringU8.Empty, new Vector2(buttonWidth, frameHeight), buttonBackground);
+            }
+
+            var pos = Im.Item.LowerRightCorner - Im.Style.FramePadding;
+            pos.Y -= Im.Style.TextHeight;
+            if (!tooltip.IsEmpty && !TooltipIcon.IsEmpty)
+            {
+                pos.X -= tooltipIconWidth;
+                Im.Window.DrawList.Text(AwesomeIcon.Font, AwesomeIcon.Font.Size, pos, TooltipIconColor.CheckDefault(ImGuiColor.TextDisabled),
+                    TooltipIcon.Span);
+                pos.X -= Im.Style.ItemInnerSpacing.X;
+            }
+
+            pos.X -= textWidth;
+            Im.Window.DrawList.Text(pos, textColor, text);
+        }
+        else
+        {
+            Im.Cursor.X += centerDistance + buttonWidth;
+        }
     }
 }
