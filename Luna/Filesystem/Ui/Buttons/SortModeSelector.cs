06434d1c23c8099763ce325aa633015a8f965fa3
Ottermandias <nickroederspam+git1@gmail.com>
2026-07-30 15:09:41 +0200

    Add the global sort mode selector.

--- Luna/Filesystem/Ui/Buttons/SortModeSelector.cs
+++ Luna/Filesystem/Ui/Buttons/SortModeSelector.cs
@@ -19,3 +19,27 @@ public class SortModeSelector(FileSystemDrawer drawer) : BaseButton<IFileSystemF
         return true;
     }
 }
+
+/// <summary> A menu selector for the global sort-mode. </summary>
+/// <param name="drawer"> The parent file system drawer. </param>
+/// <param name="configSetter"> An additional method to invoke when the sort mode is changed, e.g. to update the configuration. </param>
+public class GlobalSortModeSelector(FileSystemDrawer drawer, Action<ISortMode>? configSetter) : BaseButton
+{
+    /// <inheritdoc/>
+    public override ReadOnlySpan<byte> Label
+        => "Global Sort Mode"u8;
+
+    /// <inheritdoc/>
+    public override bool DrawMenuItem()
+    {
+        LunaStyle.DrawSeparator();
+        Im.Text("Global Sorting:"u8);
+        if (!SortModeCombo.DrawCombo(drawer.ValidSortModes, "##sortCombo"u8, drawer.SortMode, out var newSortMode, false,
+                180 * Im.Style.GlobalScale))
+            return false;
+
+        drawer.SortMode = newSortMode!;
+        configSetter?.Invoke(newSortMode!);
+        return true;
+    }
+}
